4d0acbc8302d3ee46d76a2e735d8a0ccbc584086
samhammond87 <sam.hammond1987@gmail.com>
2020-11-10 18:50:53 +1100

    delete method added to flights controller

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -24,6 +24,24 @@ class FlightsController < ApplicationController
     def edit
     end
 
+    def update
+        @flight = Flight.find(params[:id])
+       
+        if @flight.update(flight_params)
+          redirect_to @flight
+        else
+          render 'edit'
+        end
+    end
+
+
+    def destroy
+        @flight = Flight.find(params[:id])
+        @flight.destroy
+    
+        redirect_to flights_path
+    end
+
     private
   def flight_params
     params.require(:flight).permit(:title, :description)
