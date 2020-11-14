42c9e5b07787d81fae606ffbc93c484c4d3bdf6d
samhammond87 <sam.hammond1987@gmail.com>
2020-11-14 15:36:42 +1100

    SAFE POINT. About to add 'Orders' model to the database

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,6 @@ class CommentsController < ApplicationController
 
     private
         def comment_params
-          params.require(:comment).permit(:body)
+          params.require(:comment).permit(:commenter, :body)
         end
 end
-# :commenter, 
