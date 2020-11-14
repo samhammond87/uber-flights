901e007cd668a5b33ae1539ab1735ec6c3963392
samhammond87 <sam.hammond1987@gmail.com>
2020-11-14 16:06:16 +1100

    validation added to flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,4 +4,10 @@ class Flight < ApplicationRecord
   has_one_attached :flight_picture
   validates :title, presence: true,
   length: { minimum: 5 }
+  validates :cost, numericality: { greater_than_or_equal_to: 0 },
+  presence: true
+  validates :duration, numericality: { greater_than: 0 },
+  presence: true
+  validates :capacity, numericality: { greater_than: 0 },
+  presence: true
 end
