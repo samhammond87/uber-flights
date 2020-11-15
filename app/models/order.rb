bf97d5adf81c753ba6d20a205fff7a46a39e8ce0
samhammond87 <sam.hammond1987@gmail.com>
2020-11-15 13:31:54 +1100

    moved orders to nested attribute of users

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,14 +2,15 @@ class Order < ApplicationRecord
   before_validation :set_total!
   belongs_to :user
   has_many :carts
-  has_many :flights, through: :carts
+  has_many :flights
+  # , through: :carts
 
-  validates :total, presence: true, 
-  numericality: { greater_than_or_equal_to: 0 }
+  # validates :total, presence: true, 
+  # numericality: { greater_than_or_equal_to: 0 }
 
   validates :user_id, presence: true
 
-  def set_total!
-    self.total = flights.map(&:cost).sum
-  end
+  # def set_total!
+  #   self.total = flights.map(&:cost).sum
+  # end
 end
