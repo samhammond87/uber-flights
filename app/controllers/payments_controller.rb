2ab2bd877644ec45d01b895123540a18376a5fea
samhammond87 <sam.hammond1987@gmail.com>
2020-11-17 13:28:08 +1100

    Stripe adds total together & checkout button implemented

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,14 +1,22 @@
 class PaymentsController < ApplicationController
     skip_before_action :verify_authenticity_token, only: [:webhook]
     def success
+        p "......................"
+        # cart.cart_flights.purchased_at = timestamp
+        # Time.now 
+        p params
+        p "*******************"
+        p params[:cart]
     end
 
     def webhook
         payment_id = params[:data][:object][:payment_intent]
         payment = Stripe::PaymentIntent.retrieve(payment_id)
         flight_id = payment.metadata.flight_id
+        
         #user_id = payment.metadata.user_id
-        p "Flight id " + flight_id
+        p "Cart id" + cart_id
+        
         #p "user id " + user_id
         # add new order model here
         render plain: "Success"
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,10 +2,4 @@ class Cart < ApplicationRecord
   # self.table_name = 'carts'
   belongs_to :flight
   belongs_to :user
-
-  def total_cost
-    flight.cost.to_i * quantity.to_i
-  end
-
-  
 end
